20864887b36e07adc483bdeab56197611c49c2ca
ejoc1103 <ejoc1103@gmail.com>
2025-01-10 17:39:48 -0500

    Theme

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Stitches By Shelly</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.wrapper {
+  background-color: #d4a59a;
+}
+
+.wrapper h1 {
+  color: #8e6c88;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,70 @@
+<script setup>
+// {
+//   "primary": "#F5EFE6",
+//   "secondary": "#D4A59A",
+//   "accent": "#8E6C88",
+//   "background": "#FAF8F3",
+//   "textPrimary": "#333333",
+//   "textSecondary": "#666666",
+//   "button": "#C5B3A3",
+//   "buttonHover": "#A08776"
+// }
+</script>
+
+<template>
+  <main>
+    <div id="divOne">
+      <h1>Home View</h1>
+      <div id="divThree"></div>
+      <div id="divFour"></div>
+      <div id="divFive"></div>
+      <div id="divSix"></div>
+      <div id="divSeven"></div>
+      <div id="divEight"></div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+#divOne {
+  background-color: #c5b3a3;
+}
+#divOne h1 {
+  color: #f5efe6;
+}
+#divTwo {
+  background-color: #d4a59a;
+  height: 100px;
+  width: 100px;
+}
+#divThree {
+  background-color: #8e6c88;
+  height: 100px;
+  width: 100px;
+}
+#divFour {
+  background-color: #faf8f3;
+  height: 100px;
+  width: 100px;
+}
+#divFive {
+  background-color: #333333;
+  height: 100px;
+  width: 100px;
+}
+#divSix {
+  background-color: #666666;
+  height: 100px;
+  width: 100px;
+}
+#divSeven {
+  background-color: #c5b3a3;
+  height: 100px;
+  width: 100px;
+}
+#divEight {
+  background-color: #a08776;
+  height: 100px;
+  width: 100px;
+}
+</style>
