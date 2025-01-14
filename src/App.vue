41351e11bf1c7c9e918a5a067100741bf4827047
ejoc1103 <ejoc1103@gmail.com>
2025-01-13 21:01:37 -0500

    File uploads started

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
   <header>
     <div class="wrapper">
@@ -16,6 +12,10 @@ import { RouterLink, RouterView } from "vue-router";
 
   <RouterView />
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
 
 <style scoped>
 .wrapper {
--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,7 +1,17 @@
-<script></script>
-
 <template>
   <h1>Create Stitch</h1>
+  <UploadImage />
 </template>
+
+<script>
+import chart from "../assets/chart.js";
+import UploadImage from "../components/UploadImage.vue";
+
+export default {
+  components: {
+    UploadImage,
+  },
+};
+</script>
 
 <style></style>
