aabdccf395a1de2b94b43934be3b0298581f55fa
ejoc1103 <ejoc1103@gmail.com>
2025-03-11 16:49:32 -0400

    More grid work

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -13,13 +13,13 @@
     />
     <div v-if="startFile">
       <h1>File Uploded</h1>
-      <button>Pixelate Image</button>
-      <button>Set Image Grid</button>
+      <button @click="createPattern">Get Pattern Data</button>
+      <button @click="getGridImage">Set Image Grid</button>
     </div>
-    <div v-if="processedImage">
-      <button @click="changeColors">Change Colors</button>
+    <div v-if="startImage">
+      <button @click="limitColors">Get Color Pallet</button>
     </div>
-    <div v-if="colorData.length > 0">
+    <!-- <div v-if="colorData.length > 0">
       <div
         v-for="color in colorData"
         :key="color.index"
@@ -30,7 +30,7 @@
         {{ color.floss }}
         <h1>{{ color.symbol }}</h1>
       </div>
-    </div>
+    </div> -->
     <!-- Display Processed Image -->
     <!-- <div v-if="startImage">
       <h3>Starter Image</h3>
@@ -40,27 +40,47 @@
       <h3>Processed Image:</h3>
       <img :src="processedImage" alt="Modified Image" class="pixel" />
     </div>
-    <div v-if="gridImage">
+    -->
+    <!-- <div v-if="gridImage">
       <h3>Display with grid</h3>
       <img :src="gridImage" alt="Grid Image" class="pixel" />
     </div> -->
 
     <!-- Save Button -->
     <!-- <button v-if="processedImage" @click="downloadImage">Download Image</button> -->
-  </div>
-  <div
-    v-if="threadData"
-    class="newImage"
-    :style="{ gridTemplateColumns: `repeat(${maxX}, 1fr)` }"
-  >
-    <div
-      v-for="thread in threadData"
-      :key="thread.hex"
-      :style="{
-        backgroundColor: `#${thread.hex}`,
-      }"
-      class="block"
-    ></div>
+    <div v-if="patternData.length > 0" class="pattern-container">
+      <div
+        v-for="page in patternData"
+        :style="{
+          display: `grid`,
+        }"
+      >
+        <div
+          v-for="stitch in page.withSymbols"
+          :style="{
+            display: 'grid',
+            gridTemplateColumns: `repeat(${page.maxX}, 1fr)`,
+            backgroundColor: `#${stitch.hex}`,
+          }"
+        >
+          {{ stitch.symbol }}
+        </div>
+      </div>
+    </div>
+    <!-- <div
+      v-if="threadData"
+      class="newImage"
+      :style="{ gridTemplateColumns: `repeat(${maxX}, 1fr)` }"
+    >
+      <div
+        v-for="thread in threadData"
+        :key="thread.hex"
+        :style="{
+          backgroundColor: `#${thread.hex}`,
+        }"
+        class="block"
+      ></div>
+    </div> -->
   </div>
 </template>
 
@@ -94,14 +114,15 @@ export default {
       myFiles: [],
       startFile: null,
       startImage: null,
-      processedImage: null,
       gridImage: null,
       // x and y positions and hex code
       pixelData: [],
+      patternData: [],
       // hex code and matching floss #
       threadData: [],
       colorData: [],
       maxX: null,
+      maxY: null,
     };
   },
   methods: {
@@ -133,21 +154,11 @@ export default {
 
         this.startImage = await image.getBase64("image/jpeg");
 
-        image.pixelate(22);
-        this.processedImage = await image.getBase64("image/jpeg");
-
         for (let y = 0; y < height - gridSize; y += gridSize) {
           for (let x = 0; x < width - gridSize; x += gridSize) {
             const colorCount = {};
 
             image.scan(x, y, gridSize, gridSize, function (px, py, idx) {
-              // Sets the grid over the image during the scan will need to be adjusted or moved
-              // if (x % 1320 === 0 || y % 1980 === 0) {
-              //   this.bitmap.data[idx] = 0;
-              //   this.bitmap.data[idx + 1] = 0;
-              //   this.bitmap.data[idx + 2] = 0;
-              // }
-
               let r = this.bitmap.data[idx];
               let g = this.bitmap.data[idx + 1];
               let b = this.bitmap.data[idx + 2];
@@ -163,7 +174,7 @@ export default {
             const predominantColor = Object.entries(colorCount).sort(
               (a, b) => b[1] - a[1]
             )[0][0];
-
+            //Can I add symbol here?
             tempData.push({ hex: predominantColor, x: trackX, y: trackY });
             trackX++;
           }
@@ -172,14 +183,36 @@ export default {
           trackX = 0;
         }
 
-        this.pixelData = tempData;
+        this.maxY = trackY;
 
-        const base64 = await image.getBase64("image/jpeg");
-        this.gridImage = base64;
-        console.log(this.maxX + "     maxX");
+        this.pixelData = tempData;
       } catch (e) {
         console.error("Error loading image with Jimp:", e);
       }
+    },
+    async getGridImage() {
+      const image = await Jimp.read(this.startFile);
+
+      image.pixelate(22);
+
+      image.scan(
+        0,
+        0,
+        image.bitmap.width,
+        image.bitmap.height,
+        function (x, y, idx) {
+          // Sets the grid over the image during the scan will need to be adjusted or moved
+
+          if (x % 1320 === 0 || y % 1980 === 0) {
+            this.bitmap.data[idx] = 0;
+            this.bitmap.data[idx + 1] = 0;
+            this.bitmap.data[idx + 2] = 0;
+          }
+        }
+      );
+
+      const base64 = await image.getBase64("image/jpeg");
+      this.gridImage = base64;
     },
     downloadImage() {
       const link = document.createElement("a");
@@ -189,7 +222,7 @@ export default {
       link.click();
       document.body.removeChild(link);
     },
-    changeColors() {
+    limitColors() {
       let tempColors = [];
       function hexToRgb(hex) {
         const bigint = parseInt(hex.slice(1), 16);
@@ -235,7 +268,7 @@ export default {
           });
         }
 
-        if (Math.abs(currentMin - minDistance) < 15) {
+        if (Math.abs(currentMin - minDistance) < 20) {
           closest = currentClosest;
         }
         let symbol = symbols[tempColors.length];
@@ -244,6 +277,7 @@ export default {
           hex: closest.hex,
           symbol,
         });
+        this.pixelData[i].hex = closest.hex;
         if (tempColors.indexOf(closest.hex) === -1) {
           this.colorData.push({
             floss: closest.floss,
@@ -253,9 +287,76 @@ export default {
           tempColors.push(closest.hex);
         }
       }
+    },
+    createPattern() {
+      console.log(this.colorData[0]);
+      console.log(this.threadData[0]);
       console.log(this.colorData.length);
-      console.log(this.pixelData);
       console.log(this.threadData.length);
+      let colors = this.colorData;
+      // 60 X 90 blocks 5400
+      // Has to be be able to account for partial pages
+      let pages = Math.ceil(this.maxY / 90) * Math.ceil(this.maxX / 60);
+
+      let temp = [];
+
+      const width = 60;
+      const height = 90;
+
+      let xCount = 60;
+      let yCount = 90;
+
+      let xStart = 0;
+      let yStart = 0;
+
+      for (let i = 0; i < pages; i++) {
+        temp = this.pixelData.filter((thread) => {
+          if (
+            thread.x < xCount &&
+            thread.y < yCount &&
+            thread.x >= xStart &&
+            thread.y >= yStart
+          ) {
+            return thread;
+          }
+        });
+
+        let withSymbols = getSymbols(temp);
+
+        this.patternData.push(withSymbols);
+
+        xStart += width;
+        xCount += width;
+        if (xStart > this.maxX) {
+          yStart += height;
+          yCount += height;
+          xStart = 0;
+        }
+        temp = [];
+      }
+
+      function getSymbols(page) {
+        let withSymbols = [];
+
+        page.forEach((stitch) => {
+          let currentSymbol = "x";
+
+          colors.forEach((color) => {
+            if (color.hex === stitch.hex) {
+              currentSymbol = color.symbol;
+            }
+          });
+          withSymbols.push({
+            x: stitch.x,
+            y: stitch.y,
+            symbol: currentSymbol,
+            hex: stitch.hex,
+          });
+        });
+
+        let maxX = withSymbols.sort((a, b) => a.x - b.x)[0].x;
+        return { withSymbols, maxX };
+      }
     },
   },
   components: {
@@ -267,6 +368,9 @@ export default {
 <style>
 .create {
   display: grid;
+  grid-template-columns: 1fr;
+  max-width: 100vw;
+  justify-content: center;
 }
 .block {
   height: 10px;
@@ -276,5 +380,10 @@ export default {
 .newImage {
   display: grid;
   justify-self: center;
+}
+.pattern-container {
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-content: center;
 }
 </style>
